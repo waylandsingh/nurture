02f2352cfd43526563c8996e24f799a8b3d69335
Wayland Singh <wmsingh@ucdavis.edu>
2023-04-25 11:39:08 -0400

    basic frontend layout and initial comit

---
+++ client/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const App = props => {
+    return (
+      <div className="Application Container">
+        Hello there!
+      </div>
+    );
+  };
+  
+  export default App;
